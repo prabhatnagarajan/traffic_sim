8f222845c588cea09b77fbf08412d9689b915d1b
Prabhat <prabhat.nagarajan@gmail.com>
2017-04-17 00:46:14 -0500

    adds car and track classes

---
+++ car.py
@@ -0,0 +1,8 @@
+class Car:
+	def __init__(self, lane, dist, direction, speed, parked):
+		self.lane = lane
+		self.dist = dist
+		self.direction = direction
+		self.speed = speed
+		self.parked = parked
+	
---
+++ track.py
@@ -0,0 +1,8 @@
+class Track:
+	def __init__(self, num_lanes, length, cars, traffic_lights, pedestrians):
+		self.num_lanes = num_lanes
+		self.length = length
+		self.cars = cars
+		self.traffic_lights = traffic_lights
+		self.pedestrians
+
