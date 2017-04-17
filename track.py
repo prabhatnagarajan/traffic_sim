3351793a02cc3034725059d2c082cce612d61f59
Prabhat <prabhat.nagarajan@gmail.com>
2017-04-17 16:49:39 -0500

    adds traffic light

--- track.py
+++ track.py
@@ -1,8 +1,9 @@
 class Track:
-	def __init__(self, num_lanes, length, cars, traffic_lights, pedestrians):
+	def __init__(self, num_lanes, length, cars, traffic_lights, pedestrians, agent):
 		self.num_lanes = num_lanes
 		self.length = length
 		self.cars = cars
 		self.traffic_lights = traffic_lights
 		self.pedestrians
+		self.agent = agent
 
---
+++ traffic_light.py
@@ -0,0 +1,5 @@
+class TrafficLight:
+	def __init__(self, loc, colora, colorb):
+		self.loc = loc
+		self.colora = colora
+		self.colorb = colorb
