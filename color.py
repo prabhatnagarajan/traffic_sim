a501e03031c97af9dbd543a3e481a062010153aa
Prabhat <prabhat.nagarajan@gmail.com>
2017-04-17 17:00:22 -0500

    adds color, setup, and traffic light

---
+++ color.py
@@ -0,0 +1,5 @@
+from enum import Enum
+class Color(Enum):
+	green = 1
+	yellow = 2
+	red = 3
--- traffic_light.py
+++ traffic_light.py
@@ -1,5 +1,8 @@
+from color import *
 class TrafficLight:
-	def __init__(self, loc, colora, colorb):
-		self.loc = loc
-		self.colora = colora
-		self.colorb = colorb
+	def __init__(self, loc, lhs_color, rhs_color, green_time, yellow_time, red_time):
+		self.lhs_color = lhs_color
+		self.rhs_color = rhs_color
+		self.green_time = green_time
+		self.yellow_time = yellow_time
+		self.red_time = red_time
