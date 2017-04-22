cd23b8f61fdfa8342885e5ee4e9292f21e006b6f
Prabhat <prabhat.nagarajan@gmail.com>
2017-04-21 19:08:43 -0500

    does colission with car detection

--- agent.py
+++ agent.py
@@ -1,5 +1,8 @@
 import numpy as np
 import random
+from color import *
+from pdb import set_trace
+
 class Agent:
 	def __init__(self, lane, dist, direction, speed, track_len, training, module_num):
 		self.lane = lane
@@ -44,17 +47,42 @@ class Agent:
 		epsilon = self.epsilons[self.module_num]
 		Q = self.q_funcs[self.module_num]
 		state = self.get_state(track)
+		#epsilon greedy
 		if random.random() < epsilon:
 			speed, lane = random.choice(self.actions)
 		else:
 			speed, lane = self.actions[np.argmax(Q[state,:])]
 		return self.act(speed, lane, track)
 
-
 	def act(self, speed, lane, track):
+		ran_red = self.is_run_red_light(speed, lane, track)
+		car_collision = self.is_car_collision(speed, lane, track)
+		if ran_red:	
+			print "RAND RED"
+		if car_collision:
+			print "COLLISION" 
 		self.dist = (self.dist - speed + track.length) % track.length
 		self.lane = lane
 		return True
+
+	def is_car_collision(self, speed, lane, track):
+		positions = range((self.dist - speed + track.length) % track.length, self.dist + 1)
+		for car in track.cars:
+			if isinstance(car, Agent):
+				continue
+			if car.direction == self.direction:
+				if car.dist in positions:
+					if car.lane == lane:
+						return True
+		return False
+		
+	def is_run_red_light(self, speed, lane, track):
+		positions = range((self.dist - speed + track.length) % track.length, self.dist + 1)
+		for light in track.traffic_lights:
+			if light.color == Color.red:
+				if light.loc in positions:
+					return True
+		return False
 
 	def get_state(self, track):
 		if self.module_num == 1:
